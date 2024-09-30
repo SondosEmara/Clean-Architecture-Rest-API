45a2f690542bfadcc218a851bd1233a37f6dcfb2
sondos emara <sondos.emara2002@gmail.com>
2024-09-30 15:02:29 +0300

    Change StudentQuery Response

--- University.Application.Core/Features/Students/Queries/Models/GetAllStudentsQuery.cs
+++ University.Application.Core/Features/Students/Queries/Models/GetAllStudentsQuery.cs
@@ -5,8 +5,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using University.Domain.Layer.Enities;
+using University.Presentaion.Contracts.Bases;
+using University.Presentaion.Contracts.Features.Students.Queries.Results;
 namespace University.Presentaion.Contracts.Features.Students.Queries.Models
 {
     //The api input
-    public record GetAllStudentsQuery():IRequest<List<Student>>;
+    public record GetAllStudentsQuery():IRequest<Response<List<GetStudentListResponse>>>;
 }
