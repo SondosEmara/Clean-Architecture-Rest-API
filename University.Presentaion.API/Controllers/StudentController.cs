665e0c95a5cb098f7e9425b03603d9b8d796397a
sondos emara <sondos.emara2002@gmail.com>
2024-10-20 13:38:09 +0300

    solve error

--- University.Presentaion.API/Controllers/StudentController.cs
+++ University.Presentaion.API/Controllers/StudentController.cs
@@ -20,7 +20,6 @@ namespace University.Presentaion.API.Controllers
         //Add Auth-Service........!
         //Make Role=Admin........!
 
-        erjkfd
 
         [HttpGet(Router.StudentRouting.GetStudentsList)]
         public async Task<IActionResult> GetStudentList()
