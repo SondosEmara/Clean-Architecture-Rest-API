e71dbc19bd3db21e570318b74cac81bf02225305
sondos emara <sondos.emara2002@gmail.com>
2024-10-20 13:45:43 +0300

    CHECK PIPLINE23

--- University.Presentaion.API/Controllers/StudentController.cs
+++ University.Presentaion.API/Controllers/StudentController.cs
@@ -20,7 +20,7 @@ namespace University.Presentaion.API.Controllers
         //Add Auth-Service........!
         //Make Role=Admin........!
 
-        kmldji
+        ERRRPRKOOOOOOOO
         [HttpGet(Router.StudentRouting.GetStudentsList)]
         public async Task<IActionResult> GetStudentList()
         {
